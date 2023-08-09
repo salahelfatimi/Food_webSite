0d674a38138c2d8ab95bc8944b1c296473271ae2
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-08-09 17:18:52 +0100

    add .env

--- src/function/index.js
+++
@@ -1,5 +0,0 @@
-export const getServerSideProps = async () => {
-    const res = await fetch('https://api.github.com/repos/vercel/next.js')
-    const repo = await res.json()
-    return { props: { repo } }
-  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
-// export const getServerSideProps = async (id) => {
-//     const res = await fetch(`https://hypermetropic-pyram.000webhostapp.com/wp-json/wp/v2/posts/1`)
-//     const repo = await res.json()
-//     return { props: { repo } }
-//   }
+
 
 const Index = ({repo}) => {
     return(
         <div>
-            <div className=" ">
+            <div className=" text">
                 Home
             </div>
         </div>
